8fb596927f2285a825bf9651e4409e75a08d044d
Ilya.P <ilyanchikk@mail.ru>
2025-04-20 16:50:57 +0300

    add class03

---
+++ class03/1/class_1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Calculator {
+private:
+	double num1, num2;
+public:
+	Calculator(double num1, double num2) {
+		this->num1 = num1;
+		this->num2 = num2;
+	}
+	 double add() { return num1 + num2; }
+	 double multiply() { return num1 * num2; }
+	 double subtract_1_2() { return num2 - num1; }
+	 double subtract_2_1() { return num1 - num2; }
+	 double divide_1_2() { return num1 / num2; }
+	 double divide_2_1() { return num2 / num1; }
+	 bool set_num1(double num1) { 
+		 this->num1 = num1; 
+		 if (this->num1 != 0) { return true; } 
+	 }
+	 bool set_num2(double num2) {
+		 this->num2 = num2;
+		 if (this->num2 != 0) { return true; }
+	 }
+};
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double num1{}, num2{};
+	do {
+		std::cout << "Введите первое число: ";
+		std::cin >> num1;
+		std::cout << std::endl;
+		std::cout << "Введите второе число: ";
+		std::cin >> num2;
+		std::cout << std::endl;
+		Calculator calc(num1, num2);
+		if (calc.set_num1(num1) == true && calc.set_num2(num2) == true) {
+			std::cout << "num1 + num2: " << calc.add() << std::endl;
+			std::cout << "num1 * num2: " << calc.multiply() << std::endl;
+			std::cout << "num2 - num1: " << calc.subtract_1_2() << std::endl;
+			std::cout << "num1 - num2: " << calc.subtract_2_1() << std::endl;
+			std::cout << "num1 / num2: " << calc.divide_1_2() << std::endl;
+			std::cout << "num2 / num1: " << calc.divide_2_1() << std::endl;
+		}
+		else std::cout << "Введены недопустимые данные" << std::endl;
+	} while ( 1 > 0 );
+
+	return EXIT_SUCCESS;
+}
---
+++ class03/2/class_2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+char change{};
+
+class Count {
+private:
+	 int num {};
+public:
+	Count(int num) {
+		this->num = num;
+	}
+	Count() {
+		this->num = 1;
+	}
+	void plus() { ++num; }
+	void minus() { --num; }
+	void view() { std::cout << "Текущее показание счетчика: " << num << std::endl; }
+	void play() {
+		do {
+			std::cout << "Введите команду ('+', '-', '=' или 'x'):";
+			std::cin >> change;
+			switch (change) {
+			case '+': plus(); break;
+			case '-': minus(); break;
+			case 'x': break;
+			case '=': view(); break;
+			}
+		} while (change != 'x');
+	}
+};
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::string countS;
+	int countStart {};
+
+		std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+		std::cin >> countS;
+		if (countS == "да") {
+			std::cout << "Введите начальное значение счетчика: ";
+			std::cin >> countStart;
+			Count count(countStart);
+			count.play();
+			std::cout << "До свидания" << std::endl;
+		}
+		else if (countS == "нет") {
+			Count count;
+			count.play();
+			std::cout << "До свидания" << std::endl;
+		}
+		else std::cout << "Введено недопустимое значение." << std::endl;
+	
+	return EXIT_SUCCESS;
+}
