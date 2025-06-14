ec10d6d4762bdd09b10265b0b6224b0e914bffcb
Ilya.P <ilyanchikk@mail.ru>
2025-06-14 10:43:56 +0300

    change 07_1

--- 07 Препроцессор и макросы/1/07_1.cpp
+++ 07 Препроцессор и макросы/1/07_1.cpp
@@ -1,16 +1,18 @@
-﻿#include <iostream>
-#include <Windows.h>
-
-void add(int num1, int num2);
-
-#define MODE 1 
+﻿#define MODE 1
 #ifdef MODE 
+#include <iostream>
+#include <Windows.h>
+#if MODE == 0
+	int main() {
+		SetConsoleCP(1251);
+		SetConsoleOutputCP(1251);
+		std::cout << "Работаю в режиме тренировки" << std::endl;
+	}
+#elif MODE == 1
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-#if MODE == 0
-	std::cout << "Работаю в режиме тренировки" << std::endl;
-#elif MODE == 1
+	void add(int num1, int num2);
 	int num1, num2{};
 	std::cout << "Работаю в боевом режиме" << std::endl;
 	std::cout << "Введите первое число: " << std::endl;
@@ -21,14 +23,17 @@ int main() {
 	std::cout << std::endl;
 	add(num1, num2);
 	return 0;
-#else 
-	std::cout << "Неизвестный режим, завершение работы." << std::endl;
-#endif
 }
-#else
-#error Директива MODE не определена, определите MODE
-#endif
-
 void add(int num1, int num2) {
 	std::cout << "Результат сложения: " << num1 + num2 << std::endl;
 }
+#else 
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::cout << "Неизвестный режим, завершение работы." << std::endl;
+}
+#endif
+#else
+#error Директива MODE не определена, определите MODE
+#endif
