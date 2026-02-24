358a2e3adfafbb8ee76792005b04d0c2983dd58e
Ilya <ilyanchikk@mail.ru>
2026-02-24 21:31:51 +0300

    add 07

---
+++ cppl/07_1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <functional>
+#include <vector>
+#include <algorithm>
+
+int main() {
+
+	std::string str = "Hello world!!";
+
+	std::cout << "[IN] " << str << std::endl;
+	std::map<char, int> dict;
+
+	for (char c : str) {
+		int count{ 0 };
+		for (char k : str) {
+			if (c == k) {
+				count++;
+			}
+			dict[c] = count;
+		}
+		count = 0;
+	}
+
+	std::vector<std::pair<char, int>> vect(dict.begin(), dict.end());
+
+	std::sort(vect.begin(), vect.end(), [](const auto& a, const auto& b) {
+		return a.second > b.second; 
+		});
+
+	std::cout << "[OUT] " << std::endl;
+	for (const auto& pair : vect) {
+		std::cout << pair.first << ": " << pair.second << std::endl;
+	}
+
+	return 0;
+}
---
+++ cppl/07_2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <algorithm>
+#include <set>
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int size{ };
+
+	std::cout << "Введите количество элементов: ";
+	std::cin >> size;
+	std::cout << std::endl;
+	
+	std::set<int, std::greater<int>> un_num;
+
+	std::cout << "Введите элементы: " << std::endl;
+	for (int i = 0; i < size; i++) {
+		int tmp{};
+		std::cin >> tmp;
+		un_num.insert(tmp);
+	}
+
+	std::cout << "[OUT]: " << std::endl;
+	for (int n : un_num) {
+		std::cout << n  << std::endl;
+	}
+
+	return 0;
+}
