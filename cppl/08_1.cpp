0554594f73d70a330721dfb541ccb3179b46a5b2
Ilya <ilyanchikk@mail.ru>
2026-02-27 19:02:58 +0300

    add 08

---
+++ cppl/08_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+void del_dubl(std::vector<int>& vect);
+void print_vect(std::vector<int>& vect);
+
+int main() {
+	std::vector<int> vect{ 1,1,2,5,6,1,2,4 };
+	print_vect(vect);
+	del_dubl(vect);
+	print_vect(vect);
+	return 0;
+}
+void del_dubl(std::vector<int>& vect) {
+	std::sort(vect.begin(), vect.end());
+	vect.erase(unique(vect.begin(), vect.end()), vect.end());
+}
+void print_vect(std::vector<int>& vect) {
+	for (int i : vect) {
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
---
+++ cppl/08_2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <list>
+
+template <typename T>
+void print_container(T& container);
+
+int main() {
+	std::vector<int> vect1{ 1,1,5,5 };
+	std::set<int> set1{ 1,2,3,4 };
+	std::list<int> list1{ 3,3,4,4 };
+	std::vector<double> vect2{ 1.5,2.3,3.3,4.3 };
+	std::vector<std::string> vect3{ "hello","olleh" };
+	std::vector<char> vect4{ 'a','b','c' };
+	print_container(vect1);
+	print_container(set1);
+	print_container(list1);
+	print_container(vect2);
+	print_container(vect3);
+	print_container(vect4);
+
+	return 0;
+}
+
+template <typename T>
+void print_container(T& container) {
+	std::for_each(container.begin(), container.end(), [](const auto elem) {std::cout << elem << " "; }); 
+	std::cout << std::endl;
+}
